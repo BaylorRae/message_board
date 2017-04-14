d90633d127d0733cd84e3ea3019cab538ed1588b
Baylor Rae <baylor@thecodedeli.com>
2017-04-14 12:47:26 -0500

    fix bug: remove default order by

--- app/models/post_listing.rb
+++ app/models/post_listing.rb
@@ -1,8 +1,6 @@
 class PostListing < ActiveRecord::Base
   self.primary_key = :id
 
-  default_scope { order(:created_at) }
-
   belongs_to :user
   has_many :messages, class_name: 'PostMessage', foreign_key: :post_id
 
