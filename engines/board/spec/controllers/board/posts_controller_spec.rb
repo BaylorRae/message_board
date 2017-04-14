3e1f323d2de1637039709448a6dea85b1855dc0e
Baylor Rae <baylor@thecodedeli.com>
2017-04-14 14:18:35 -0500

    stub method chain for includes.find

--- engines/board/spec/controllers/board/posts_controller_spec.rb
+++ engines/board/spec/controllers/board/posts_controller_spec.rb
@@ -5,19 +5,19 @@ describe Board::PostsController do
 
   context "GET show" do
     it "finds a post by id" do
-      expect(Post).to receive(:find).with('post-id')
+      expect(Post).to receive_message_chain(:includes, :find).with('post-id')
       get :show, id: 'post-id'
     end
 
     it "assigns the post" do
       post = double(:post)
-      allow(Post).to receive(:find).with('post-id') { post }
+      allow(Post).to receive_message_chain(:includes, :find).with('post-id') { post }
       get :show, id: 'post-id'
       expect(assigns(:post)).to eq(post)
     end
 
     it "renders a 404 nicely" do
-      allow(Post).to receive(:find).with('post-id') { raise ActiveRecord::RecordNotFound }
+      allow(Post).to receive_message_chain(:includes, :find).with('post-id') { raise ActiveRecord::RecordNotFound }
       get :show, id: 'post-id'
       expect(response).to render_template('404')
     end
