e24da4d1e0439a060ef91a40823be0fcf80a060b
Baylor Rae <baylor@thecodedeli.com>
2017-04-14 09:18:25 -0500

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+ADJECTIVES = %w(salty sweet happy sad cheerful spooky).freeze
+NOUNS      = %w(apples bananas car squid walrus cheese).freeze
+
+users = User.create!([
+  { email: 'bob@example.com', password: 'secret' },
+  { email: 'alice@example.com', password: 'secret' },
+  { email: 'leo@example.com', password: 'secret' }
+])
+
+posts = users.map { |user| Post.create!(title: "#{ADJECTIVES.sample}-#{NOUNS.sample}-#{Random.rand(100) * 77}", user: user) }
+
+posts.each do |post|
+  ActiveRecord::Base.transaction do
+    15.times do |i|
+      time = i.minutes.ago
+      post.messages.create!(user: users.sample, body: ADJECTIVES.sample(2).join(' ') + ' ' + NOUNS.sample, created_at: time, updated_at: time)
+    end
+  end
+end
